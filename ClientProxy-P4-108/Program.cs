c1bed25f0f6cae9057236e030996c8501a4f6714
KinanthieMPutrie <kinanthieputrie@gmail.com>
2020-11-07 16:28:51 +0800

    Add project files.

---
+++ ClientProxy-P4-108/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientProxy_P4_108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServiceReference1.MatematikaClient obj = new
+            ServiceReference1.MatematikaClient();
+                double hasilTambah = obj.Tambah(1, 2);
+                Console.WriteLine("1 + 2 = " + hasilTambah);
+                double hasilKurang = obj.Kurang(3, 2);
+                Console.WriteLine("3 - 2 = " + hasilKurang);
+                double hasilKali = obj.Kali(2, 2);
+                Console.WriteLine("2 x 2 = " + hasilKali);
+                double hasilBagi = obj.Bagi(2, 0);
+                Console.WriteLine("2 / 2 = " + hasilBagi);
+
+                ServiceReference1.Koordinat a = new ServiceReference1.Koordinat();
+                ServiceReference1.Koordinat b = new ServiceReference1.Koordinat();
+                a.X = 7;
+                a.Y = 8;
+
+                b.X = 5;
+                b.Y = 6;
+
+                double selisihX = a.X - b.X;
+                double selisihY = a.Y - b.Y;
+
+                double jarak = Math.Sqrt(Math.Pow(selisihX, 2) + Math.Pow(selisihY, 2));
+                Console.WriteLine("Hasil Koordinat " + jarak);
+            }
+            catch(FaultException<ServiceReference1.MathFault> mf)
+            {
+                Console.WriteLine(mf.Detail.Kode);
+                Console.WriteLine(mf.Detail.Pesan);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
